47d4eee4b5fd84f6397b1b7218e7bc555a74fecc
blessedchitamba <blessedbrighton@live.com>
2018-07-21 19:13:02 +0200

    some progress with the picture carousel. Trying out different javascript approaches

--- blessed.js
+++ blessed.js
@@ -11,6 +11,10 @@
 	    }
 	    slideIndex++;
 	    if (slideIndex > x.length) {slideIndex = 1} 
-	    x[slideIndex-1].style.display = "block"; 
-	    setTimeout(carousel, 5000); // Change image every 2 seconds
+	   // x[slideIndex-1].classList.add("activePic"); 
+		x[slideIndex-1].style.display = "block";
+		x[slideIndex-1].style.maxWidth = "70%";
+		x[slideIndex-1].style.maxHeight = "70%";
+		x[slideIndex-1].style.margin = "auto"; 
+	    setTimeout(carousel, 3000); // Change image every 2 seconds
 	}
