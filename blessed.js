b49630de37e24454d3ead62b785c18f481a99ff7
blessedchitamba <blessedbrighton@live.com>
2018-07-21 10:55:40 +0200

    now its added..lol

---
+++ blessed.js
@@ -0,0 +1,16 @@
+// Javascript file to deal with some parts of the website
+
+	var slideIndex = 0;
+	carousel();
+
+	function carousel() {
+	    var i;
+	    var x = document.getElementsByClassName("pictures");
+	    for (i = 0; i < x.length; i++) {
+	      x[i].style.display = "none"; 
+	    }
+	    slideIndex++;
+	    if (slideIndex > x.length) {slideIndex = 1} 
+	    x[slideIndex-1].style.display = "block"; 
+	    setTimeout(carousel, 5000); // Change image every 2 seconds
+	}
