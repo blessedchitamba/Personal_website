b5977c828021910309543091d45d26fb0dca65ad
blessedchitamba <blessedbrighton@live.com>
2018-07-24 20:51:20 +0200

    Finally the scrolling picture banner is working with the implementation of the classList JS functiongit add *.css *.js *.html

--- blessed.js
+++ blessed.js
@@ -5,16 +5,20 @@
 
 	function carousel() {
 	    var i;
-	    var x = document.getElementsByClassName("pictures");
+	    var x = document.getElementsByClassName("picture");
 	    for (i = 0; i < x.length; i++) {
-	      x[i].style.display = "none"; 
+	      //x[i].style.display = "none"; 
+	      x[i].classList.remove("activePic");
+	      x[i].classList.add("inactivePic");
 	    }
 	    slideIndex++;
 	    if (slideIndex > x.length) {slideIndex = 1} 
-	   // x[slideIndex-1].classList.add("activePic"); 
-		x[slideIndex-1].style.display = "block";
+	    x[slideIndex-1].classList.add("activePic"); 
+		x[slideIndex-1].classList.remove("inactivePic");
+		/*x[slideIndex-1].style.display = "block";
 		x[slideIndex-1].style.maxWidth = "70%";
 		x[slideIndex-1].style.maxHeight = "70%";
-		x[slideIndex-1].style.margin = "auto"; 
+		x[slideIndex-1].style.margin = "auto";
+		x[slideIndex-1].style.marginTop = "70px";*/
 	    setTimeout(carousel, 3000); // Change image every 2 seconds
 	}
